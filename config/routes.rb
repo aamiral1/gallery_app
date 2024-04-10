e6c0113a5f595ccbdfa96c8afb7867581a01cfb2
Aamir Ali <amohamedthahirali1@sheffield.ac.uk>
2024-04-10 20:03:06 +0100

    Add Devise User Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # GET /
-  root "main#index"
+  root to: "main#index"
 
   # GET /about
   get "about", to: "about#index"
